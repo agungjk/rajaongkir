06aade75a7b3c3707186bf7396ae4482ef34e9d3
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-10-04 10:46:10 +0700

    update cost eror
    tapi kodingan masih acak2an dan belum memenuhi standard oop

--- src/RajaOngkir.php
+++ src/RajaOngkir.php
@@ -98,15 +98,31 @@ class RajaOngkir {
 
 	public function cost($origin, $destination, $weight, $courier)
 	{
-		$options = [
-			CURLOPT_CUSTOMREQUEST => "POST",
-			CURLOPT_POSTFIELDS => "origin=". $origin ."&destination=". $destination ."&weight=". $weight ."&courier=". $courier,
-			CURLOPT_HTTPHEADER => array(
-				"content-type: application/x-www-form-urlencoded",
-		    	"key: " . self::key
-			),
-		];
-		return self::_request('/cost', $options);
-	}
+		$curl = curl_init();
+
+		    curl_setopt_array($curl, array(
+		      CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+		      CURLOPT_RETURNTRANSFER => true,
+		      CURLOPT_ENCODING => "",
+		      CURLOPT_MAXREDIRS => 10,
+		      CURLOPT_TIMEOUT => 30,
+		      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		      CURLOPT_CUSTOMREQUEST => "POST",
+		      CURLOPT_POSTFIELDS => "origin=$origin&destination=$destination&weight=$weight&courier=$courier",
+		      CURLOPT_HTTPHEADER => array(
+			"content-type: application/x-www-form-urlencoded",
+			"key: $this->key"
+		      ),
+		    ));
+
+		    $response = curl_exec($curl);
+		    $err = curl_error($curl);
+
+		    curl_close($curl);
+
+				$rajaongkir = json_decode($response, true);
+				return $rajaongkir['rajaongkir'];
+			}
+			}
 
 }
